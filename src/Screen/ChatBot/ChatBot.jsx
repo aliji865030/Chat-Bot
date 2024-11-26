473d67d4bf8d1542741bba81e6765f0d654a0780
aliji865030 <abbasalichand786@gmail.com>
2024-11-26 20:55:34 +0530

    head section done

--- src/Screen/ChatBot/ChatBot.jsx
+++ src/Screen/ChatBot/ChatBot.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import "./ChatBot.css"
+import { IoMdClose } from "react-icons/io";
+import { BsChatDotsFill } from "react-icons/bs";
 
 const ChatBot = () => {
   return (
     <div className='chat-bot'>
         <div className="head">
-            <div className="bot-name"></div>
-            <div className="close"></div>
+            <div className="bot-name">
+                <div className='icon'>
+                    <BsChatDotsFill style={{color:"#0f9bf0"}} />
+                </div>
+                <div className='bots-name'>
+                    <span>Jinn Live</span>
+                    <span className='demo'>Demo Bot</span>
+                </div>
+            </div>
+            <div className="close">
+                <IoMdClose />
+            </div>
         </div>
         <div className="chat"></div>
         <div className="foot"></div>
